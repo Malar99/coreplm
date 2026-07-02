6eaf535a2684c1a2f1881fb1f1ea2e811a0aa7bb
Malar <malars1403@gmail.com>
2026-07-02 22:40:45 +0530

    feat: add Role entity

---
+++ src/main/java/com/coreplm/entity/Role.java
@@ -0,0 +1,29 @@
+package com.coreplm.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
